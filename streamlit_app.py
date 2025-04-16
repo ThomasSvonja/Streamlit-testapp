1fa5e2f014926bcd1af3271663da24073ddccb1e
ThomasSvonja <103067671@student.swin.edu.au>
2025-04-16 20:46:39 +1000

    attempt to add pydeck map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.title("THIS IS A TEST")
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
